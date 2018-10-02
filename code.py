4422f59f735766f66a942999b610b6e2f914fab8
CANDYISLIFE <billybender@hanovernorwichschools.org>
2018-10-02 11:46:43 -0400

    Updated from Brython Server: 10/2/2018 11:46:41 AM

--- code.py
+++ code.py
@@ -1,1 +1,11 @@
+beginningSize = int(input("How big is the iceberg at first? "))
+speed = int(input("How fast are you traveling? "))
 
+days = 9600/(speed*24)
+#print(days)
+
+radius = ((beginningSize*3)/(4*3.14159265))**(1/3)
+#print(radius)
+
+for i in range(days):
+    
