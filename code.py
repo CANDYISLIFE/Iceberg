03f19f4afbeebb78ade9164f7994406b4b207812
CANDYISLIFE <billybender@hanovernorwichschools.org>
2018-10-02 11:53:10 -0400

    Updated from Brython Server: 10/2/2018 11:53:08 AM

--- code.py
+++ code.py
@@ -1,5 +1,14 @@
-beginningSize = int(input("How big is the iceberg at first? "))
+boatSize = int(input("What size boat are you renting? Say L,M, or S"))
 speed = int(input("How fast are you traveling? "))
+
+if boatSize == str("L"):
+    beginningSize = 10000000
+if boatSize == str("M"):
+    beginningSize = 100000
+if boatSize == str("S"):
+    beginningSize = 500000
+else:
+    print("Please enter either a L, M, or S ")
 
 days = 9600/(speed*24)
 #print(days)
@@ -7,5 +16,5 @@ days = 9600/(speed*24)
 radius = ((beginningSize*3)/(4*3.14159265))**(1/3)
 #print(radius)
 
-for i in range(days):
+#for i in range(days):
     
