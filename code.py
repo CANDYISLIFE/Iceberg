5d497490404e901d16d7d431c23e4997ad1655cf
CANDYISLIFE <billybender@hanovernorwichschools.org>
2018-10-02 22:03:09 -0400

    Updated from Brython Server: 10/2/2018 10:03:09 PM

--- code.py
+++ code.py
@@ -1,12 +1,15 @@
-boatSize = int(input("What size boat are you renting? Say L,M, or S"))
+boatSize = input("What size boat are you renting? Say L, M, or S ")
 speed = int(input("How fast are you traveling? "))
 
 if boatSize == str("L"):
     beginningSize = 10000000
+    rental = 1400
 if boatSize == str("M"):
     beginningSize = 100000
+    rental = 780
 if boatSize == str("S"):
     beginningSize = 500000
+    rental = 520
 else:
     print("Please enter either a L, M, or S ")
 
@@ -16,5 +19,6 @@ days = 9600/(speed*24)
 radius = ((beginningSize*3)/(4*3.14159265))**(1/3)
 #print(radius)
 
-#for i in range(days):
+for i in range(days):
+    
     
