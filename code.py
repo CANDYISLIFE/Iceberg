8503d862b5cd23ba3ceb8b7cab86cbcefce30f21
CANDYISLIFE <billybender@hanovernorwichschools.org>
2018-10-03 19:52:11 -0400

    Updated from Brython Server: 10/3/2018 7:52:11 PM

--- code.py
+++ code.py
@@ -2,7 +2,7 @@ import math
 
 boatSize = input("What size boat are you renting? Say L, M, or S ")
 speed = int(input("How fast are you traveling? Enter 1, 3, or 5 "))
-
+distance = 0
 
 if boatSize == str("L"):
     beginningSize = 10000000
@@ -22,8 +22,9 @@ if boatSize == str("S"):
 #else:
     print("Please enter either a L, M, or S ")
 
+"""
 if speed == 5:
-    fuelCost = (1.84575*math.log(beginningSize)-4.0333)
+    fuelCost = (1.84575*math.log(beginningSize)-4.0333)7
     print(fuelCost)
 if speed == 3: 
     fuelCost = (1.52003*math.log(beginningSize)-3.46667)
@@ -31,12 +32,37 @@ if speed == 3:
 if speed == 1: 
     fuelCost = (1.19431*math.log(beginningSize)-2.83333)
     print(fuelCost)
+"""
 
 days = 9600/(speed*24)
 #print(days)
 
 radius = ((beginningSize*3)/(4*3.14159265))**(1/3)
 #print(radius)
+beginningSize = radius
+#print(beginningSize)
 
-
+for i in range(int(days)):
+    if speed == 5:
+        fuelCost = (1.84575*math.log(beginningSize)-4.0333)
+        shrink = .0001(beginningSize) 
+        #print(fuelCost)
+    if speed == 3: 
+        fuelCost = (1.52003*math.log(beginningSize)-3.46667)
+        shrink = .00008(beginningSize)
+        #print(fuelCost)
+    if speed == 1: 
+        fuelCost = (1.19431*math.log(beginningSize)-2.83333)
+        shrink = .00006(beginningSize)
+        #print(fuelCost)
+    distance += (speed*24)
+    beginningSize += -shrink
+    print(beginningSize)
+        
+        
+        
+        
+        
+        
+        
     
